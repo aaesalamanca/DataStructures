753d0cbb943b20394b2101a0abd9cbd407435ea5
aaesalamanca <aaesalamanca@gmail.com>
2022-05-27 17:11:23 +0200

    Add example of using MyStack collection

--- UsingCollections/Program.cs
+++ UsingCollections/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DataStructures;
 
+UsingMyStack();
+
+static void UsingMyStack()
+{
+    var myIntStack = new MyStack<int>();
+
+    myIntStack.Push(1);
+    myIntStack.Push(2);
+    myIntStack.Push(3);
+
+    Console.WriteLine(myIntStack.Pop());
+    Console.WriteLine(myIntStack.Pop());
+    Console.WriteLine(myIntStack.Pop());
+
+    var myStringStack = new MyStack<string>();
+
+    myStringStack.Push("String1");
+    myStringStack.Push("String2");
+    myStringStack.Push("String3");
+
+    Console.WriteLine(myStringStack.Pop());
+    Console.WriteLine(myStringStack.Pop());
+    Console.WriteLine(myStringStack.Pop());
+}
